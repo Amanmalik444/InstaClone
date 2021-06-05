c640e595b20c62075341fe3d35c0d5ba35d112f4
Aman malik <amanmaliknhr@gmail.com>
2021-06-05 18:10:45 +0530

    commenting UI fix

--- frontend/src/Routes/Posts/Comment.js
+++ frontend/src/Routes/Posts/Comment.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
 import DeleteIcon from "@material-ui/icons/Delete";
+import DeleteIconOutline from "@material-ui/icons/DeleteOutline";
 import serverLink from "../../utils/serverLink";
 import axios from "axios";
 import "./Post.css";
 
 const Comment = ({ comment, loggedInUser, refetch, postId }) => {
+  const [deletingComment, setDeletingComment] = useState(false);
+
   //deleting the comment
   const deleteComment = () => {
+    setDeletingComment(true);
     axios
       .post(`${serverLink}/post/deleteComment`, {
         commentId: comment._id,
@@ -15,6 +19,7 @@ const Comment = ({ comment, loggedInUser, refetch, postId }) => {
       .then((res) => {
         console.log(res);
         refetch();
+        setDeletingComment(false);
       })
       .catch((err) => console.log(err));
   };
@@ -26,16 +31,30 @@ const Comment = ({ comment, loggedInUser, refetch, postId }) => {
           display: "flex",
           flexDirection: "row",
           justifyContent: "space-between",
+          alignItems: "center",
           marginTop: "1vh",
           marginRight: "1vh",
           marginLeft: "1vh",
+          maxWidth: "38vh",
         }}
       >
-        <h5 style={{ color: "rgba(0.8,0.8,0.8,0.8)" }}>
+        <h5
+          style={{
+            color: "rgba(0.8,0.8,0.8,0.8)",
+            maxWidth: "34vh",
+          }}
+        >
           {comment.userId.userName} : {comment.text}
         </h5>
         {loggedInUser._id === comment.userId._id ? (
-          <DeleteIcon style={{ cursor: "pointer" }} onClick={deleteComment} />
+          deletingComment === false ? (
+            <DeleteIconOutline
+              style={{ cursor: "pointer" }}
+              onClick={deleteComment}
+            />
+          ) : (
+            <DeleteIcon style={{ cursor: "pointer" }} onClick={deleteComment} />
+          )
         ) : (
           ""
         )}
