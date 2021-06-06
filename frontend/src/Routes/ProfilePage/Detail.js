0a7c02b8e0dd66cf6f25d704721639b90e598313
Aman malik <amanmaliknhr@gmail.com>
2021-06-06 16:52:16 +0530

    new buttons on profile

--- frontend/src/Routes/ProfilePage/Detail.js
+++ frontend/src/Routes/ProfilePage/Detail.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import Button from "@material-ui/core/Button";
 import Input from "@material-ui/core/Input";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import InstagramIcon from "@material-ui/icons/Instagram";
 import { useHistory } from "react-router-dom";
 import serverLink from "../../utils/serverLink";
 import { Image, Transformation, CloudinaryContext } from "cloudinary-react";
@@ -24,6 +25,7 @@ const Detail = ({
   const [caption, setCaption] = useState("");
   const [loading, setLoading] = useState(false);
   const [deleting, setDeleting] = useState(false);
+  const [posting, setPosting] = useState(false);
   const [loadingValue, setLoadingValue] = useState(0);
 
   const history = useHistory();
@@ -122,7 +124,7 @@ const Detail = ({
           color="primary"
           style={{
             margin: "10px",
-            width: "200px",
+            width: "130px",
           }}
           onClick={() => setDeleting(true)}
         >
@@ -130,17 +132,38 @@ const Detail = ({
           Delete user{" "}
         </Button>
       ) : (
-        <Button
-          variant="outlined"
-          color="secondary"
+        <div
           style={{
-            margin: "10px",
-            width: "200px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
           }}
-          onClick={deleteUser}
         >
-          confirm
-        </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            style={{
+              margin: "10px",
+              width: "130px",
+              marginBottom: "5px",
+            }}
+            onClick={deleteUser}
+          >
+            confirm
+          </Button>
+          <Button
+            variant="contained"
+            color="default"
+            style={{
+              margin: "10px",
+              marginTop: "0px",
+            }}
+            onClick={() => setDeleting(false)}
+          >
+            cancel
+          </Button>
+        </div>
       )}
       <p
         style={{
@@ -152,67 +175,93 @@ const Detail = ({
         {bio}{" "}
       </p>
 
-      <form onSubmit={Upload}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <label htmlFor="uploadPhoto">
-            <Input
-              // label="Enter Image"
-              type="file"
-              id="uploadPhoto"
-              name="image"
-              accept="image/*"
-              onChange={(e) => setImage(e.target.files[0])}
-              style={{
-                display: "none",
-              }}
-            />
-            <Button
-              color="dark"
+      {posting === true ? (
+        <form onSubmit={Upload}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <label htmlFor="uploadPhoto">
+              <Input
+                // label="Enter Image"
+                type="file"
+                id="uploadPhoto"
+                name="image"
+                accept="image/*"
+                onChange={(e) => setImage(e.target.files[0])}
+                style={{
+                  display: "none",
+                }}
+              />
+              <Button
+                color="dark"
+                variant="outlined"
+                component="span"
+                style={{
+                  margin: "3px",
+                  width: "200px",
+                  marginTop: "2vh",
+                }}
+              >
+                Select Image
+              </Button>
+            </label>
+            <TextField
+              label="Enter Caption"
               variant="outlined"
-              component="span"
+              onChange={(e) => setCaption(e.target.value)}
               style={{
                 margin: "3px",
+                marginTop: "1vh",
+                marginBottom: "1vh",
                 width: "200px",
-                marginTop: "2vh",
               }}
-            >
-              Select Image
-            </Button>
-          </label>
-          <TextField
-            label="Enter Caption"
-            variant="outlined"
-            onChange={(e) => setCaption(e.target.value)}
-            style={{
-              margin: "3px",
-              marginTop: "1vh",
-              marginBottom: "1vh",
-              width: "200px",
-            }}
-          />
-          {loading === false && postsFetched === true ? (
-            <Button
-              variant="outlined"
-              color="primary"
-              type="submit"
-              style={{
-                width: "100px",
-              }}
-            >
-              Upload
-            </Button>
-          ) : (
-            <CircularProgress variant="determinate" value={loadingValue} />
-          )}
-        </div>
-      </form>
+            />
+            {loading === false && postsFetched === true ? (
+              <div>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  type="submit"
+                  style={{
+                    margin: "3px",
+                  }}
+                >
+                  <InstagramIcon />
+                </Button>
+                <Button
+                  variant="contained"
+                  color="default"
+                  type="submit"
+                  style={{
+                    margin: "3px",
+                  }}
+                  onClick={() => setPosting(false)}
+                >
+                  Cancel
+                </Button>
+              </div>
+            ) : (
+              <CircularProgress variant="determinate" value={loadingValue} />
+            )}
+          </div>
+        </form>
+      ) : (
+        <Button
+          variant="outlined"
+          color="primary"
+          style={{
+            margin: "10px",
+          }}
+          onClick={() => setPosting(true)}
+        >
+          Post on instagram
+        </Button>
+      )}
     </div>
   );
 };
